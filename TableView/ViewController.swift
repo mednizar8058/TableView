90a882efe07a6479c93f3df6ebb6782ba4c37c5b
MedNizar <rockstarhard4@gmail.com>
2021-05-30 17:12:44 +0100

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by MNizar on 5/30/21.
+//  Copyright © 2021 MNizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
