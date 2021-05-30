4d4e1edc6d2bc3c60d7f6892f50ad741aa89a424
MedNizar <rockstarhard4@gmail.com>
2021-05-30 21:52:42 +0100

    initializing with table views

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var students = [("ali","ali"),("ahmed","ahmed"),("sara","sara")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "i'm a cell"
+        return cell
+    }
+    
+    
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+
+    
 
 
 }
