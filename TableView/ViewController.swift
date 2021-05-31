e28eb7f5e2bd6c64010a0e5e727ee41d8f8d1f14
MedNizar <rockstarhard4@gmail.com>
2021-05-31 09:32:52 +0100

    adding sections

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -9,25 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
-    var students = [("Ahmed","first year"),("Sara","second year"),("Ali","first year"),("Ayoub","third year")]
+    var UH1students = [("Ahmed","UH1"),("Sara","UH1"),("Ali","UH1"),("Ayoub","UH1")]
+    var EnsaStudents = [("kamal","ENSA"),("brahim","ENSA"),("jawad","ENSA")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
     }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return students.count
+        if section == 0{
+            return UH1students.count
+        }
+        else{
+            return EnsaStudents.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if indexPath.section == 0{
+            let (student,_) = UH1students[indexPath.row]
+            cell.textLabel?.text = student
+        }
+        else{
+            let (student,_) = EnsaStudents[indexPath.row]
+            cell.textLabel?.text = student
+        }
         
-        cell.textLabel?.text = students[indexPath.row].0
-        cell.detailTextLabel?.text = students[indexPath.row].1
+        
+        
         return cell
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0{
+            return "UH1"
+        }
+        else {
+            return "ENSA"
+        }
+    }
     
 
    
